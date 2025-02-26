31d08e8e9ed4a2f3d102891e1d781670d2745460
mauricio <mauriciogtec@gmail.com>
2025-02-26 17:31:41 -0500

    added pysqlite3 dep

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,13 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_openai.embeddings import OpenAIEmbeddings
 from omegaconf import OmegaConf
-               
-                                         
+
+
+# Needed for streamlist
+__import__("pysqlite3")
+import sys
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
 # Setup Streamlit's page config
 st.set_page_config(page_title="Mauricio Tec's Live CV")
 st.title("Mauricio Tec's Live CV")
